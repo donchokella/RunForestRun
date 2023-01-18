fe02e51d0dd425e5dc07c8b455963b39a7ef5754
donchokella <ali.alparslan@outlook.com>
2023-01-18 14:25:37 +0300

    Player and opponents go to end

---
+++ Assets/Scripts/OpponentController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OpponentController : MonoBehaviour
+{
+    public NavMeshAgent OpponentAgent;
+    public GameObject Target;
+    private void Start()
+    {
+        OpponentAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    private void Update()
+    {
+        OpponentAgent.SetDestination(Target.transform.position);
+    }
+}
