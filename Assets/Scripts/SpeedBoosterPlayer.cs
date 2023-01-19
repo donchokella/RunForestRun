1748fe6890ec141705c961a473719381a28cc563
donchokella <ali.alparslan@outlook.com>
2023-01-19 12:41:32 +0300

    SpeedBooster for player and opponent

---
+++ Assets/Scripts/SpeedBoosterPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpeedBoosterPlayer : MonoBehaviour
+{
+    //public NavMeshAgent OpponentAgent;
+    public PlayerController playerController;
+    //public OpponentController opponentController;
+
+    private void Start()
+    {
+        //OpponentAgent = GetComponent<NavMeshAgent>();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Booster"))
+        {
+            playerController.runningSpeed += 3f;
+            //opponentController.OpponentAgent.speed += 3f;
+            StartCoroutine(SlowDownAfeterCoroutine());
+        }
+    }
+
+    private IEnumerator SlowDownAfeterCoroutine()
+    {
+        yield return new WaitForSeconds(2f);
+        playerController.runningSpeed -= 3f;
+        //opponentController.OpponentAgent.speed -= 3f;
+    }
+}
